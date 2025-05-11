ed6e5baf6dc04c1ade24f15b8c7655a40cdbea52
silvany <207448498+vany-garsos@users.noreply.github.com>
2025-05-11 16:11:43 -0600

    se modifica el menu para el usuario administrador

--- admin/vistas/modulos/menu.php
+++ admin/vistas/modulos/menu.php
@@ -36,105 +36,92 @@
                             <i class="fas fa-fw fa-chart-area"></i>
                             <span>Mostrar Usuarios</span></a>
                     </li>
+
+                    <!----------- LOGO-------------->
+                    <hr class="sidebar-divider">
+
+                    <!-- Heading -->
+                    <div class="sidebar-heading">
+                        Logo
+                    </div>
+                    <!-- Nav Item - Charts -->
+                    <li class="nav-item">
+                        <a class="nav-link" href="logo">
+                            <i class="fas fa-fw fa-chart-area"></i>
+                            <span>Mostrar Logo</span></a>
+                    </li>
+
+                    <!----------- PRODUCTOS-------------->
+                    <hr class="sidebar-divider">
+
+                    <!-- Heading -->
+                    <div class="sidebar-heading">
+                        Productos
+                    </div>
+                    <!-- Nav Item - Charts -->
+                    <li class="nav-item">
+                        <a class="nav-link" href="productos">
+                            <i class="fas fa-fw fa-chart-area"></i>
+                            <span>Mostrar Productos</span></a>
+                    </li>
+
+                    <!----------- UBICACION-------------->
+                    <hr class="sidebar-divider">
+
+                    <!-- Heading -->
+                    <div class="sidebar-heading">
+                        Ubicacion
+                    </div>
+                    <!-- Nav Item - Charts -->
+                    <li class="nav-item">
+                        <a class="nav-link" href="ubicacion">
+                            <i class="fas fa-fw fa-chart-area"></i>
+                            <span>Mostrar Ubicacion</span></a>
+                    </li>
+
+                    <!----------- SLIDER-------------->
+                    <hr class="sidebar-divider">
+                    <!-- Heading -->
+                    <div class="sidebar-heading">
+                        Sliders
+                    </div>
+                    <!-- Nav Item - Charts -->
+                    <li class="nav-item">
+                        <a class="nav-link" href="sliders">
+                            <i class="fas fa-fw fa-chart-area"></i>
+                            <span>Mostrar Sliders</span></a>
+                    </li>
+
+                    <!----------- PIE DE PAGINA-------------->
+                    <hr class="sidebar-divider">
+                    <!-- Heading -->
+                    <div class="sidebar-heading">
+                        Pie de Pagina
+                    </div>
+                    <!-- Nav Item - Charts -->
+                    <li class="nav-item">
+                        <a class="nav-link" href="piepagina">
+                            <i class="fas fa-fw fa-chart-area"></i>
+                            <span>Mostrar Pie de pagina</span></a>
+                    </li>
+
+                    <!----------- NOSOTROS-------------->
+                    <hr class="sidebar-divider">
+                    <!-- Heading -->
+                    <div class="sidebar-heading">
+                        Seccion nosotros
+                    </div>
+                    <!-- Nav Item - Charts -->
+                    <li class="nav-item">
+                        <a class="nav-link" href="nosotros">
+                            <i class="fas fa-fw fa-chart-area"></i>
+                            <span>Nosotros</span></a>
+                    </li>
+
             <?php
                 }
             }
             ?>
-
-            <!----------- LOGO-------------->
-            <hr class="sidebar-divider">
-
-            <!-- Heading -->
-            <div class="sidebar-heading">
-                Logo
-            </div>
-            <!-- Nav Item - Charts -->
-            <li class="nav-item">
-                <a class="nav-link" href="logo">
-                    <i class="fas fa-fw fa-chart-area"></i>
-                    <span>Mostrar Logo</span></a>
-            </li>
-
-
-
-            <!----------- PRODUCTOS-------------->
-            <hr class="sidebar-divider">
-
-            <!-- Heading -->
-            <div class="sidebar-heading">
-                Productos
-            </div>
-            <!-- Nav Item - Charts -->
-            <li class="nav-item">
-                <a class="nav-link" href="productos">
-                    <i class="fas fa-fw fa-chart-area"></i>
-                    <span>Mostrar Productos</span></a>
-            </li>
-
-
-
-            <!----------- UBICACION-------------->
-            <hr class="sidebar-divider">
-
-            <!-- Heading -->
-            <div class="sidebar-heading">
-                Ubicacion
-            </div>
-            <!-- Nav Item - Charts -->
-            <li class="nav-item">
-                <a class="nav-link" href="ubicacion">
-                    <i class="fas fa-fw fa-chart-area"></i>
-                    <span>Mostrar Ubicacion</span></a>
-            </li>
-
-
-
-            <!----------- SLIDER-------------->
-            <hr class="sidebar-divider">
-            <!-- Heading -->
-            <div class="sidebar-heading">
-                Sliders
-            </div>
-            <!-- Nav Item - Charts -->
-            <li class="nav-item">
-                <a class="nav-link" href="sliders">
-                    <i class="fas fa-fw fa-chart-area"></i>
-                    <span>Mostrar Sliders</span></a>
-            </li>
-
-
-
-            <!----------- PIE DE PAGINA-------------->
-            <hr class="sidebar-divider">
-            <!-- Heading -->
-            <div class="sidebar-heading">
-                Pie de Pagina
-            </div>
-            <!-- Nav Item - Charts -->
-            <li class="nav-item">
-                <a class="nav-link" href="piepagina">
-                    <i class="fas fa-fw fa-chart-area"></i>
-                    <span>Mostrar Pie de pagina</span></a>
-            </li>
-
-
-
-
-            <!----------- NOSOTROS-------------->
-            <hr class="sidebar-divider">
-            <!-- Heading -->
-            <div class="sidebar-heading">
-                Seccion nosotros
-            </div>
-            <!-- Nav Item - Charts -->
-            <li class="nav-item">
-                <a class="nav-link" href="nosotros">
-                    <i class="fas fa-fw fa-chart-area"></i>
-                    <span>Nosotros</span></a>
-            </li>
-
-
-
 
             <!-- Divider -->
             <hr class="sidebar-divider d-none d-md-block">
